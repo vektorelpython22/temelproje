cbbfc1c063040a0db6f507781d2f0db59e802e7c
vektorelpython22Ofis <vektorelpython@gmail.com>
2020-09-19 12:46:27 +0300

    19092020_3

---
+++ IBRAHIM/Banka.py
@@ -0,0 +1,4 @@
+from Tools.FileTools import FileTool
+banka = FileTool(isim=r"IBRAHIM\Banka",alan=["Banka Adı",\
+    "Hesap Tür","Bakiye"])
+banka.menu()
--- IBRAHIM/Tools/FileTools.py
+++ IBRAHIM/Tools/FileTools.py
@@ -18,9 +18,9 @@ class FileTool:
         """
         from os import path
         if path.exists(self.isim):
-            self.dosya = open(self.isim,"r+")
+            self.dosya = open(self.isim,"r+",encoding="UTF-8")
         else:
-            self.dosya = open(self.isim,"w+")
+            self.dosya = open(self.isim,"w+",encoding="UTF-8")
         self.dosyaLines = self.dosya.readlines()
     
     def listeleme(self,liste=[]):
@@ -59,14 +59,36 @@ class FileTool:
     def kaydet(self):
         self.dosya.seek(0)
         self.dosya.truncate()
-        self.dosya.writeline(self.dosyaLines)
+        self.dosya.writelines(self.dosyaLines)
         self.dosya.flush()
     
 
     def __del__(self):
         self.dosya.seek(0)
         self.dosya.truncate()
-        self.dosya.writeline(self.dosyaLines)
+        self.dosya.writelines(self.dosyaLines)
         self.dosya.close()
 
     
+    def menu(self):
+        menu = f""" {self.isim} üzerinde çalışılıyor
+        1-Ekleme
+        2-Güncelleme
+        3-Silme
+        4-Listeleme
+        5-Kaydetme
+        6-Çıkış
+        """ 
+        anahtar = 1
+        while anahtar == 1:
+            islem = int(input(menu))
+            liste=["",self.ekleme,self.guncelleme,\
+                self.silme,self.listeleme,self.kaydet]
+            if islem == 6:
+                anahtar = 0
+            else:
+                liste[islem]()
+        
+if __name__ == "__main__":
+    obj1 = FileTool(isim=r"IBRAHIM\teldefter")
+    obj1.menu()
