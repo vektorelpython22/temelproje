d526edb30d8166ce29b191b887e0b007589bbc55
İbrahim EDİZ <vektorelpython22@gmail.com>
2020-10-04 10:11:47 +0300

    04102020

--- IBRAHIM/OPENCV/Opencv2.py
+++ IBRAHIM/OPENCV/Opencv2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread(r"IBRAHIM\OPENCV\resimler\baska.png",cv2.IMREAD_UNCHANGED)
-imgGri = cv2.imread(r"IBRAHIM\OPENCV\resimler\baska.png",cv2.IMREAD_GRAYSCALE)
+
 
 
 buyuk_perc = 20
@@ -12,6 +12,8 @@ boyut = (en,boy)
 
 resized = cv2.resize(img,boyut,interpolation=cv2.INTER_AREA)
 
+
+cv2.imwrite(r"IBRAHIM\OPENCV\resimler\duzeltilmis.jpg",resized)
 cv2.imshow("image",resized)
 # cv2.imshow("imageGri",imgGri)
 
