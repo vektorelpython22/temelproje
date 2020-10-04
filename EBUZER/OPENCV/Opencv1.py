677e7ce9a9bee7f85e2c23694154ba5851b1155c
Ebuz <ebuz.vektor@gmail.com>
2020-10-04 11:26:20 +0300

    push push push
    I'm Lord ya ya ya

---
+++ EBUZER/OPENCV/Opencv1.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img = cv2.imread(r"IBRAHIM\OPENCV\resimler\baska.png",cv2.IMREAD_UNCHANGED)
+imgGri = cv2.imread(r"IBRAHIM\OPENCV\resimler\baska.png",cv2.IMREAD_GRAYSCALE)
+
+
+buyuk_perc = 20
+en = int(img.shape[1] * buyuk_perc/100)
+boy = int(img.shape[0] * buyuk_perc/100)
+boyut = (en,boy)
+
+resized = cv2.resize(img,boyut,interpolation=cv2.INTER_AREA)
+
+cv2.imshow("image",resized)
+# cv2.imshow("imageGri",imgGri)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- EBUZER/OPENCV/opencv2.py
+++ EBUZER/OPENCV/opencv2.py
@@ -1,20 +1,21 @@
 import cv2
 import numpy as np
-img = cv2.imread(r"EBUZER\OPENCV\resimler\baska.png",cv2.IMREAD_UNCHANGED)
-imgGri = cv2.imread(r"EBUZER\OPENCV\resimler\baska.png",cv2.IMREAD_GRAYSCALE)
+
+img = cv2.imread(r"IBRAHIM\OPENCV\resimler\baska.png",cv2.IMREAD_UNCHANGED)
+
 
 
 buyuk_perc = 20
-width = int(img.shape[1]*buyuk_perc/100)
-height = int(img.shape[0]*buyuk_perc/100)
-dim =(width,height)
-resized = cv2.resize(img,dim,interpolation=cv2.INTER_AREA)
+en = int(img.shape[1] * buyuk_perc/100)
+boy = int(img.shape[0] * buyuk_perc/100)
+boyut = (en,boy)
 
+resized = cv2.resize(img,boyut,interpolation=cv2.INTER_AREA)
+
+
+cv2.imwrite(r"IBRAHIM\OPENCV\resimler\duzeltilmis.jpg",resized)
 cv2.imshow("image",resized)
-# cv2.imshow("imageGri",imgGri)
-cv2.imwrite(r"EBUZER\OPENCV\resimler\duzeltilmis.jpg",resized)
-cv2.imshow("image",resized)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
